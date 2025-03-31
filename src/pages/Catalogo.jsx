5cb34812d7e97366eecd2c701a24ba9cf081bd38
JercOmg <juan1j2u3an@gmail.com>
2025-03-30 23:30:45 -0500

    arreglo de bug

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -13,7 +13,7 @@ export default function Catalogo() {
         <br />
 
         <div className="flex justify-center">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 justify-items-center">
             <TractorCard
               imageSrc={Tractor}
               link="/CatalogoTractor"
